f33cebbc3f20d17b173126071dba8e73e1f76f06
Natalie Funk <nfunk@sfu.ca>
2019-02-22 23:33:38 -0800

    Fixed syntax error

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,16 +3,17 @@
 dir_items=$(ls | wc -l)  # get the number of items in the current directory
 
 function get_guess {
-	echo "Enter a number."
+	echo "Please enter a number:"
 	read response
 	echo "You entered: $response"
 }
 
+clear
 echo "Guess how many files are in the current directory."
 get_guess
 
 # compare numeric response to dir_items
-while [[ $response -ne $dir_items]]
+while [[ $response -ne $dir_items ]]
 do
 	# if response < dir_items 
 	if [[ $response -lt $dir_items ]]
