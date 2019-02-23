22915bc3e1d7936b018cfd2f380053e04f6217d5
Natalie Funk <nfunk@sfu.ca>
2019-02-22 21:16:24 -0800

    outlined initial logic for shell program

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,14 @@
+echo "Guess how many files are in the current directory. Please enter a number."
+read response
 
+
+
+echo "You entered: $response"
+
+echo "Your answer is too low."
+
+echo "Your answer is too high."
+
+echo "Please guess again. Enter a number."
+
+echo "Your guess is correct. You are very clever. Congratulations!"
